c09cfff2f2d66ac5ab2cbb7018917ab19a48b2fc
KirbyLink <kjryan.reeves@gmail.com>
2018-10-27 23:02:19 -0700

    Added prefix/postfix for picking up scarecrow

--- PumpkinKing/PumpkinKing.cs
+++ PumpkinKing/PumpkinKing.cs
@@ -2,13 +2,8 @@
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewValley;
-using StardewValley.Locations;
 using StardewValley.TerrainFeatures;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
 
 namespace PumpkinKing
 {
@@ -25,16 +20,21 @@ namespace PumpkinKing
             //Harmony patcher
             //https://github.com/KirbyLink/PumpkinKing
             var harmony = HarmonyInstance.Create("com.github.kirbylink.pumpkinking");
-            var original = typeof(HoeDirt).GetMethod("plant");
-            var prefix = helper.Reflection.GetMethod(typeof(PumpkinKing.PumpkinKingModifications), "Prefix").MethodInfo;
-            var postfix = helper.Reflection.GetMethod(typeof(PumpkinKing.PumpkinKingModifications), "Postfix").MethodInfo;
-            harmony.Patch(original, new HarmonyMethod(prefix), new HarmonyMethod(postfix));
+            var plantOriginal = typeof(HoeDirt).GetMethod("plant");
+            var plantPrefix = helper.Reflection.GetMethod(typeof(PumpkinKing.PlantNearPumpkinKing), "Prefix").MethodInfo;
+            var plantPostfix = helper.Reflection.GetMethod(typeof(PumpkinKing.PlantNearPumpkinKing), "Postfix").MethodInfo;
+            var removeOriginal = typeof(Tool).GetMethod("DoFunction");
+            var removePrefix = helper.Reflection.GetMethod(typeof(PumpkinKing.PickUpPumpkinKing), "Prefix").MethodInfo;
+            var removePostfix = helper.Reflection.GetMethod(typeof(PumpkinKing.PickUpPumpkinKing), "Postfix").MethodInfo;
+            harmony.Patch(plantOriginal, new HarmonyMethod(plantPrefix), new HarmonyMethod(plantPostfix));
+            harmony.Patch(removeOriginal, new HarmonyMethod(removePrefix), new HarmonyMethod(removePostfix));
 
         }
     }
 
-    public static class PumpkinKingModifications
+    public static class PlantNearPumpkinKing
     {
+        
         /* Check if planting fertilizer or if the crops has no seasons to grow in or not pumpkins */
         static void Prefix(HoeDirt __instance, bool isFertilizer, int index, int tileX, int tileY, ref bool __state)
         {
@@ -80,21 +80,141 @@ namespace PumpkinKing
                     {
                         /* Code from HoeDirt.plant() */
                         int numGrowthDays = 0;
-                        for (int index1 = 0; index1 < crop.phaseDays.Count - 1; ++index1)
+                        for (int index1 = 0; index1 < crop.phaseDays.Count - 1; index1++)
                             numGrowthDays += crop.phaseDays[index1];
                         bool hasOtherModifier = __instance.fertilizer.Value == 465 || __instance.fertilizer.Value == 466 || who.professions.Contains(5);
                         int growthDayChange = (hasOtherModifier ? 3 : 4);
+                        int initialOffset = 0;
+                        //Set initialOffset based on last phaseDay modified by fertilizer/skill combo
+                        if (hasOtherModifier)
+                        {
+                            if (__instance.fertilizer.Value == 465)
+                            {
+                                initialOffset = (who.professions.Contains(5) ? 4 : 3);
+                            }
+                            else if (__instance.fertilizer.Value == 466)
+                            {
+                                initialOffset = (who.professions.Contains(5) ? 3 : 2);
+                            }
+                            else
+                                initialOffset = 3;
+                        }
+
                         for (int i1 = 0; growthDayChange > 0 && i1 < 3; ++i1)
                         {
-                            for (int i2 = 0; i2 < crop.phaseDays.Count; ++i2)
+                            int i2 = (i1 == 0 ? initialOffset : 0);
+                            while (i2 < crop.phaseDays.Count - 1)
                             {
-                                if (i2 > 0 || crop.phaseDays[i2] > 1)
+                                if (crop.phaseDays[i2] > 1)
                                 {
                                     crop.phaseDays[i2]--;
                                     --growthDayChange;
                                 }
-                                if (growthDayChange <= 0)
+                                if (growthDayChange <= 0 || (i2 == crop.phaseDays.Count - 2 && crop.phaseDays[i2] == 1))
+                                {
                                     break;
+                                }
+                                i2++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static class PickUpPumpkinKing
+    {
+
+        /* Check if item being removed is The Pumpkin King */
+        static void Prefix(Tool __instance, GameLocation location, int x, int y, ref bool __state)
+        {
+            if (__instance != null && location.IsFarm)
+            {
+                Vector2 vector = new Vector2((float)(x / 64), (float)(y / 64));
+                StardewValley.Object vectorObject = null;
+                if (location.objects.ContainsKey(vector))
+                {
+                    vectorObject = location.objects[vector];
+                }
+                __state = vectorObject != null && vectorObject.Name.Contains("The Pumpkin King");
+            }
+        }
+
+        static void Postfix(GameLocation location, int x, int y, Farmer who, bool __state)
+        {
+            /*  
+             *  Remove 25% growth bonus from surrounding pumpkins
+            */
+
+            //If state is true, we removed a Pumpkin King on the farm
+            if (__state)
+            {
+                //Find all HoeDirt within radius
+                foreach (KeyValuePair<Vector2, TerrainFeature> pair in location.terrainFeatures.Pairs)
+                {
+                    TerrainFeature pairTerrain = pair.Value;
+                    Vector2 scarecrowVector = new Vector2((float)(x / 64), (float)(y / 64));
+                    bool isHoeDirt = pairTerrain is HoeDirt;
+                    bool isPumpkin = (isHoeDirt ? ((pairTerrain as HoeDirt).crop != null && (pairTerrain as HoeDirt).crop.indexOfHarvest.Value == 276) : false);
+                    bool isWithinRange = Vector2.Distance(scarecrowVector, pair.Key) < 9.0;
+                    //Increase phaseDays by 25%
+                    if (isPumpkin && isWithinRange)
+                    {
+                        HoeDirt hoeDirt = (pairTerrain as HoeDirt);
+                        Crop crop = hoeDirt.crop;
+                        bool hadOtherModifier = hoeDirt.fertilizer.Value == 465 || hoeDirt.fertilizer.Value == 466 || who.professions.Contains(5);
+                        int phaseDayCount = crop.phaseDays.Count - 1;                        
+                        //Reset phaseDays based on last phaseDay modified by fertilizer/skill combo
+                        if (hadOtherModifier)
+                        {
+                            if (hoeDirt.fertilizer.Value == 465 || who.professions.Contains(5))
+                            {
+                                if (hoeDirt.fertilizer.Value == 465 && who.professions.Contains(5))
+                                {
+                                    crop.phaseDays[phaseDayCount - 1] = 3;
+                                    crop.phaseDays[phaseDayCount - 2] = 3;
+                                    crop.phaseDays[phaseDayCount - 3] = 2;
+                                }
+                                else
+                                {
+                                    crop.phaseDays[phaseDayCount - 1] = 3;
+                                    crop.phaseDays[phaseDayCount - 2] = 4;
+                                    crop.phaseDays[phaseDayCount - 3] = 2;
+                                }
+                            }
+                            else 
+                            {
+                                if (who.professions.Contains(5))
+                                {
+                                    crop.phaseDays[phaseDayCount - 1] = 2;
+                                    crop.phaseDays[phaseDayCount - 2] = 3;
+                                }
+                                else
+                                {
+                                    crop.phaseDays[phaseDayCount - 1] = 2;
+                                    crop.phaseDays[phaseDayCount - 2] = 3;
+                                    crop.phaseDays[phaseDayCount - 3] = 2;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for(int i = 1; i < crop.phaseDays.Count - 1; i++)
+                            {
+                                switch (i)
+                                {
+                                    case 1:
+                                        crop.phaseDays[i] = 2;
+                                        break;
+                                    case 3:
+                                        crop.phaseDays[i] = 4;
+                                        break;
+                                    case 2:
+                                    case 4:
+                                        crop.phaseDays[i] = 3;
+                                        break;
+                                }
                             }
                         }
                     }
